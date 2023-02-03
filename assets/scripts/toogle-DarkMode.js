eedb161c411191a1a586917a4c8ebdd6ba38a9e3
Claudinei Martins <claudineimartins8@gmail.com>
2023-02-02 22:13:25 -0300

    Update toogle-DarkMode.js

--- assets/scripts/toogle-DarkMode.js
+++ assets/scripts/toogle-DarkMode.js
@@ -4,18 +4,36 @@ const botaoMudar = document.querySelector ("#alterador");
             
 
             const body =document.querySelector("body");
-            const nomeModo = document.querySelector("#nomeModo");
-
+            const sections = document.querySelectorAll(".js-Altera-Bg");
+            const textos=document.querySelectorAll(".js-altera-color");
+            const nomeModo = document.querySelector("#nomeModo"); //Mostra se esta em dark ou ligh na pagina//
+            
             if (botaoMudar.checked==true){
-                body.style.background = "#000000" ;  
+               
+                body.style.background = "#0D0D0D" ;  
                 body.style.color = "#FFFFFF";
-                nomeModo.innerHTML = 'Light Mode'
+                nomeModo.innerHTML = 'Light Mode';
+                sections.forEach(section => {
+                    section.style.background = "#1A1A1A";
+                  });
+
+
+                textos.forEach(texto=> {
+                    texto.style.color ="#FFFFFF"
+                });
+                  
+                  
+                  
          
             }
             else {
                 body.style.background = "#FFFFFF";
                 body.style.color = "#000000";
-                nomeModo.innerHTML = 'Dark Mode'
+                nomeModo.innerHTML = 'Dark Mode';
+                sections.forEach(section => {
+                    section.style.background = "#F0F2F4";
+                  });
+                  
             }
         }
         botaoMudar.addEventListener("click", Alterar)
